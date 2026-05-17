f492e994285015533e79526fbf73903207e8ff33
Florin Badita <baditaflorin@gmail.com>
2026-05-17 20:22:40 +0300

    chore: rebuild docs for mesh-common v0.3.0 (10 sensor primitives + 13 sensor refactors + vitest dedupe fix across 74 apps)

--- src/Feature.tsx
+++ src/Feature.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import {
+  MeshNameInput,
   QRExchange,
   commit,
   randomSalt,
@@ -157,10 +158,10 @@ function Body({ room, config }: { room: YRoom; config: MeshConfig }) {
         </p>
       </header>
 
-      <input
+      <MeshNameInput
         className="viral-name"
         value={name}
-        onChange={(e) => setName(e.target.value)}
+        onChange={setName}
         placeholder="your name"
         maxLength={48}
       />
